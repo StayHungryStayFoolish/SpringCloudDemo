fce3edbf36c7dc0c56a6f058a4a9d12da35b90db
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-04 11:24:28 +0800

    commit

--- service-feign-hystrix/src/main/java/com/springcloud/ServiceFeignHystrixApplication.java
+++ service-feign-hystrix/src/main/java/com/springcloud/ServiceFeignHystrixApplication.java
@@ -4,13 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * @EnableDiscoveryClient
  * 向服务中心注册
  * @EnableFeignClients
  * 注解会开启 Feign 功能
+ * @EnableHystrixDashboard
+ * 注解会开启断路器监控器
+ * URL: http://localhost:8767/hystrix
  */
+@EnableHystrixDashboard
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
