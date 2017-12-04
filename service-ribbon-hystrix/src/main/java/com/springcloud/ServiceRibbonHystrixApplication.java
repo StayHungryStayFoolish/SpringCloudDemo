9d4979c7134e59dee49259975f41dbe7aa4d2212
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-04 11:39:13 +0800

    commit

--- service-ribbon-hystrix/src/main/java/com/springcloud/ServiceRibbonHystrixApplication.java
+++ service-ribbon-hystrix/src/main/java/com/springcloud/ServiceRibbonHystrixApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
  * @EnableHystrix
  * 该注解开启 Hystrix 断路器
  */
+@EnableHystrixDashboard
 @EnableHystrix
 @EnableDiscoveryClient
 @SpringBootApplication
