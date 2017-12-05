750c782e1cf971f79c0d77c7a30d389e961bacb6
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-05 18:30:55 +0800

    commit

--- eureka-client/src/main/java/com/srpingcloud/DcController.java
+++ eureka-client/src/main/java/com/srpingcloud/DcController.java
@@ -36,10 +36,20 @@ public class DcController {
     /**
      * getServices(); 源码是 List
      *
+     * Thread.sleep(); 为了触发 Hystrix 的服务降级，简单加5秒延迟
+     * 场景模拟触发 Hystrix 服务降级：
+     * 1.启动 Eureka-Server、Eureka-Client、Eureka-Ribbon-Hystrix。
+     * 2.当访问 localhost:8766/hi?name=spring-ribbon-hystrix，会触发服务降级。
+     * 因为服务提供方输出了原本要返回的结果，但是由于返回前延迟了5秒，所以服务消费方触发了
+     * 服务请求超时异常，服务消费者就通过 HystrixCommand 注解中指定的降级逻辑进行执行。
+     *
+     * 该机制，对自身服务起到了基础的保护，同时还为异常情况提供了自动的服务降级切换机制。
+     *
      * @return
      */
     @GetMapping("/dc")
-    public String dc() {
+    public String dc() throws InterruptedException {
+        Thread.sleep(5000L);
         String services = "Services : " + discoveryClient.getServices();
         System.out.println("Services --> "+services);
         return services;
