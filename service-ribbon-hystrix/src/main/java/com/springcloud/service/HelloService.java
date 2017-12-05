0b0d19c10ddad552e1ae48b3897ea23d58da4d95
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-05 18:08:27 +0800

    commit

--- service-ribbon-hystrix/src/main/java/com/springcloud/service/HelloService.java
+++ service-ribbon-hystrix/src/main/java/com/springcloud/service/HelloService.java
@@ -15,11 +15,16 @@ public class HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
+    // @HystrixCommand 包装方法函数时，除了定义服务降级外，
+    // Hystrix 框架会自动为这个函数实现调用隔离。
+    // 依赖隔离、服务降级 在使用适合是一体化实现的。
+    // 利用 Hystrix 来实现服务容错保护在编程模型上非常方便。
     @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://EUREKA-CLIENT/hi?name=" + name, String.class);
     }
 
+    // 服务降级
     public String hiError(String name) {
         return "Hi , " + name + " Sorry, Error !";
     }
