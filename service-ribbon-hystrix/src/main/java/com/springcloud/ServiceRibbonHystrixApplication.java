c1dc9ca0cd69565ca9457d5cd15397ca793a7980
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-05 17:43:32 +0800

    commit

--- service-ribbon-hystrix/src/main/java/com/springcloud/ServiceRibbonHystrixApplication.java
+++ service-ribbon-hystrix/src/main/java/com/springcloud/ServiceRibbonHystrixApplication.java
@@ -2,6 +2,7 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -14,6 +15,13 @@ import org.springframework.web.client.RestTemplate;
  * 该注解向 EurekaServer(服务中心)注册
  * @EnableHystrix
  * 该注解开启 Hystrix 断路器
+ * @SpringCloudApplication
+ * 可以代替
+ * 1、@EnableHystrix / @EnableCircuitBreaker(作用同前者)
+ * 2、@EnableDiscoveryClient
+ * 3、@SpringBootApplication 三个注解
+ *
+ * 看源码
  */
 @EnableHystrixDashboard
 @EnableHystrix
