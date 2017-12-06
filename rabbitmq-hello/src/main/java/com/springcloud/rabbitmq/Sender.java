2537d1d0cbe9a090a013817847edc073c0ab84c8
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-06 10:55:28 +0800

    commit

---
+++ rabbitmq-hello/src/main/java/com/springcloud/rabbitmq/Sender.java
@@ -0,0 +1,30 @@
+package com.springcloud.rabbitmq;
+
+import com.sun.prism.shader.Solid_TextureYV12_AlphaTest_Loader;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 上午10:51 on 17/12/6.
+ * <p>
+ * AmqpTemplate 接口定义了一套针对 AMQP 协议的基础操作。
+ * <p>
+ * 消息发送者
+ */
+@Component
+public class Sender {
+
+    // 注入 AmqpTemplate 接口，来实现消息发送
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void send() {
+        String context = "Hello" + new Date();
+        System.out.println("Sender : " + context);
+        this.rabbitTemplate.convertAndSend("Hello", context);
+    }
+}
