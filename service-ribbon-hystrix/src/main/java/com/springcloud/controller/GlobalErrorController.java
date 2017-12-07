0f19018c52e7bb325ebb1e70a6e20e957dddf9b8
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-07 10:18:36 +0800

    commit

--- service-ribbon-hystrix/src/main/java/com/springcloud/controller/GlobalErrorController.java
+++ service-ribbon-hystrix/src/main/java/com/springcloud/controller/GlobalErrorController.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
